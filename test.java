bd91f43b80f9ec88e770e7d3dccf193ccc66bfe3
codestudent2 <119500805+codestudent2@users.noreply.github.com>
2026-03-07 11:52:57 +0530

    Add a basic Java program that prints 'hello world'

---
+++ test.java
@@ -0,0 +1,8 @@
+class test{
+        public static void main(String[] args)
+        {
+                System.out.println("hello world");
+                int a=30;
+                int b=20;
+        }
+}
