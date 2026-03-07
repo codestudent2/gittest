cd511780b1e468ee03f6340759fd3796e6260d30
codestudent2 <hatstu2@gmail.com>
2026-03-07 11:57:04 +0530

    modified a value

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ class test{
         public static void main(String[] args)
         {
                 System.out.println("hello world");
-                int a=30;
+                int a=20;
                 int b=20;
         }
 }
